0505c9d47ff817a4c7f2829c0c933da07d5d150c
nam1234 <nam@njit.edu>
2024-10-01 16:48:05 -0400

    recreated test_operation.py from kaw393939/calculator_is218_f24 repo

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -1,7 +1,9 @@
-'''My Calculator Test'''
+''' My Calculator Test'''
 import pytest
-from app.operations import addition, subtraction, multiplication, division
+from app.operations import addition, division, multiplication, subtraction
 
+
+# Parameterized test for addition
 @pytest.mark.parametrize("a, b, expected", [
     (1, 1, 2), (2, 3, 5), (-1, -1, -2), (0, 0, 0)
 ])
@@ -9,28 +11,32 @@ def test_addition(a, b, expected):
     '''Addition function'''
     assert addition(a, b) == expected
 
+# Parameterized test for subtraction
 @pytest.mark.parametrize("a, b, expected", [
-    (1, 1, 0), (-1, -1, 0), (3, 1, 2), (-5, -1, -4)
+    (1, 1, 0), (5, 3, 2), (-1, -1, 0), (0, 5, -5)
 ])
 def test_subtraction(a, b, expected):
     '''Subtraction function'''
     assert subtraction(a, b) == expected
 
+# Parameterized test for multiplication
 @pytest.mark.parametrize("a, b, expected", [
-    (1, 1, 1), (3, 4, 12), (-2, -4, 8), (0, 0, 0)
+    (2, 2, 4), (3, 5, 15), (0, 5, 0), (-1, 1, -1)
 ])
 def test_multiplication(a, b, expected):
     '''Multiplication function'''
     assert multiplication(a, b) == expected
 
+# Parameterized test for division
 @pytest.mark.parametrize("a, b, expected", [
-    (2, 2, 1), (10, 5, 2), (-15, -5, 3), (-16, 4, -4)
+    (2, 2, 1), (10, 5, 2), (9, 3, 3)
 ])
-def test_positive_division(a, b, expected):
-    '''Positive Division function'''
+def test_division(a, b, expected):
+    '''Division function'''
     assert division(a, b) == expected
 
-def test_negative_division():
-    '''Negative Division function'''
+# Test for division by zero exception
+def test_division_by_zero_exception():
+    '''Division function testing that I get the exception divide by zero'''
     with pytest.raises(ZeroDivisionError):
-        division(1, 0)
+        division(10, 0)
